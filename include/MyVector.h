4b77b8bd1c1800e6cdfc359f7eaf37a1f158ae8b
APeshko <m2415803@edu.misis.ru>
2025-04-21 21:04:39 +0300

    Update MyVector.h

--- include/MyVector.h
+++ include/MyVector.h
@@ -1,38 +1,37 @@
+// MyVector.h
 #include <algorithm>
 #include <initializer_list>
 
 template <typename T>
 class MyVector {
-private:
-    T* data = nullptr;
-    size_t size = 0;
-    size_t capacity = 0;
+    T* data;
+    size_t size;
+    size_t capacity;
 
 public:
-    MyVector() = default;
-    explicit MyVector(size_t size);
+    // Конструкторы
+    MyVector() : data(nullptr), size(0), capacity(0) {}
+    explicit MyVector(size_t n) : data(new T[n]), size(n), capacity(n) {}
     MyVector(std::initializer_list<T> init);
     
-    ~MyVector();
+    // Деструктор
+    ~MyVector() { delete[] data; }
     
+    // Копирование
     MyVector(const MyVector& other);
-
     MyVector& operator=(const MyVector& other);
     
-    // Move кон
+    // Перемещение
     MyVector(MyVector&& other) noexcept;
-    
-    // Move о_п
     MyVector& operator=(MyVector&& other) noexcept;
     
     // Методы доступа
-    T& operator[](size_t index);
-    const T& operator[](size_t index) const;
-    size_t getSize() const;
-    size_t getCapacity() const;
+    T& operator[](size_t index) { return data[index]; }
+    const T& operator[](size_t index) const { return data[index]; }
+    size_t getSize() const { return size; }
+    size_t getCapacity() const { return capacity; }
     
-    // Методы модификации
-    void push_back(const T& value);
-    void push_back(T&& value);
+    // Вспомогательные методы
     void reserve(size_t newCapacity);
+    void push_back(const T& value);
 };
