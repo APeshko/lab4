687319464d1ad87d5174f4f17f6d8c72b7e8444f
APeshko <m2415803@edu.misis.ru>
2025-04-21 21:07:45 +0300

    Update MyArray.h

--- include/MyArray.h
+++ include/MyArray.h
@@ -1,26 +1,17 @@
-#include <stdexcept>
 
 template <typename T, size_t N>
 class MyArray {
-private:
     T data[N];
-
+    
 public:
-    // Доступ к элементам
-    T& operator[](size_t index) {
-        if (index >= N) throw std::out_of_range("Index out of range");
-        return data[index];
-    }
+    T& operator[](size_t index) { return data[index]; }
+    const T& operator[](size_t index) const { return data[index]; }
     
-    const T& operator[](size_t index) const {
-        if (index >= N) throw std::out_of_range("Index out of range");
-        return data[index];
-    }
-    
-    constexpr size_t size() const { return N; }
+    size_t size() const { return N; }
     
     T* begin() { return data; }
     const T* begin() const { return data; }
+    
     T* end() { return data + N; }
     const T* end() const { return data + N; }
 };
